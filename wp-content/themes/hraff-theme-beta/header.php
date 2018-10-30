16880d5ad9888b902ac0e4a70a1f435a5cc5b296
Stephanie Lai <slai27@student.monash.edu>
2018-10-30 12:20:01 +1100

    Added bootstrap into header Created front page file

--- wp-content/themes/hraff-theme-beta/header.php
+++ wp-content/themes/hraff-theme-beta/header.php
@@ -2,6 +2,7 @@
 <head>
 	<title><?php wp_title( '-', true, 'right' ); bloginfo( 'name' ); ?></title>
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <?php wp_head(); ?>
 </head>
 
