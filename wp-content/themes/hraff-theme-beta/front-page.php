9ae686232d9f2584532b3ae32826ed4419e74641
Stephanie Lai <slai27@student.monash.edu>
2018-11-01 23:44:49 +1100

    Home page layout modifications

--- wp-content/themes/hraff-theme-beta/front-page.php
+++ wp-content/themes/hraff-theme-beta/front-page.php
@@ -1,22 +1,27 @@
-<!doctype html>
-
+<!DOCTYPE html>
+<html>
 <?php get_header(); ?>
 
+<head></head>
+    <body>
+        <section>
+            
 
-<section>
-<!-- ========== START PAGE TEMPLATE ========== -->
+        <h2><?php wp_title( '' ); ?></h2>
+   
 
-  <h2><?php wp_title( '' ); ?></h2>
+<!---Carousel--->
+
     
-  <section>
-    <h3 class="intro">The Human Rights Arts and Film Festival (HRAFF) was founded in 2007 to make human rights relevant, accessible, and engaging to all Australians.</h3>
+  <div class="intro">
+    <h3>The Human Rights Arts and Film Festival (HRAFF) was founded in 2007 to make human rights relevant, accessible, and engaging to all Australians.</h3>
     <p> Welcome to the 2018 Human Rights Arts & Film Festival.
         In a ‘post-truth’ world, the powerful role creativity can
         play in opening minds and hearts can’t be underestimated.
         Through its dynamic mix of thought-provoking films, forums
         and arts events – this festival invites us to ask questions,
         explore new perspectives and learn about the experiences
-        of others in our community and beyond. <br/><br/>Martin Foley - Minister for Creative Industries</p></section>
+        of others in our community and beyond. <br/><br/>Martin Foley - Minister for Creative Industries</p></div>
     
 <!-- Film -->    
     <div id="film-program">
@@ -30,7 +35,6 @@
   <article>
     
     <?php if ( is_archive() || is_home() ) { ?>
-		<div class="home-event">
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         <div><?php the_post_thumbnail('thumbnail');?></div>
             <?php
@@ -38,15 +42,17 @@
 		} else {
 			the_content();
 		}?>
-		<div><?php the_field('event_date'); ?></div>
-		<div><?php the_field('venue_name'); ?></div>
-		<div><?php if( get_field( 'event_duration' ) ) {
-			the_field( 'event_duration' ).print(" mins");
-		}?></div>
-		<div><?php if( get_field( 'age_restriction' ) ) {
-			the_field( 'age_restriction' );
-		}?></div>
-      </div>
+            <div class="details">
+		      <div><?php the_field('event_date'); ?></div>
+		      <div><?php the_field('venue_name'); ?></div>
+		      <div><?php if( get_field( 'event_duration' ) ) {
+			     the_field( 'event_duration' ).print(" mins");
+		          }?></div>
+		      <div><?php if( get_field( 'age_restriction' ) ) {
+			     the_field( 'age_restriction' );
+		          }?></div>
+            </div>
+    
   </article>
   
   <?php endwhile; else : ?>
@@ -58,18 +64,18 @@
     </div>
 
 <!-- Art -->
-    <div>
-        <h2>Arts Program</h2>
+<div id="arts-program">
+    <h2>Arts Program</h2>
+        
     <?php 
 	query_posts( array( 'post_type' => 'events', 'event_type' => 'art', 'posts_per_page' => 3));
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); 
   ?>
   
-  
+
   <article>
-  
     <?php if ( is_archive() || is_home() ) { ?>
-		<div class="home-event">
+		
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         <div><?php the_post_thumbnail('thumbnail');?></div>
         <?php
@@ -77,12 +83,13 @@
 		} else {
 			the_content();
 		}?>
+        <div class="details">
 		<div><?php the_field('event_date'); ?></div>
         <div><?php the_field('venue_name'); ?></div>    
 		<div><?php if( get_field( 'age_restriction' ) ) {
 			the_field( 'age_restriction' );
 		}?></div>
-      </div>
+        </div>
   </article>
   
   <?php endwhile; else : ?>
@@ -92,9 +99,11 @@
   
   <?php endif; ?>
     </div>
-    
-<!-- ========== END PAGE TEMPLATE ========== -->
+     
+
 </section>
 
 
 <?php get_footer(); ?>
+    </body>
+</html>
