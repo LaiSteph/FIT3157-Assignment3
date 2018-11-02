1a127f54d418f83199e9a821cbe769c70ed335fb
Hayden <hbak0001@student.monash.edu>
2018-11-02 15:53:41 +1100

    Minor improvements to footer

--- wp-content/themes/hraff-theme-beta/footer.php
+++ wp-content/themes/hraff-theme-beta/footer.php
@@ -1,5 +1,21 @@
   <footer>
-    &copy; 2019 HRAFF - Human Rights Arts & Film Festival - hraff.org.au. <a href="http://localhost:3157/beta/contact-us">Contact Us</a><a href="http://localhost:3157/beta/privacy-policy">Privacy</a>
+  	<div class="sponsor" style="text-align: center">
+		<div style="display: inline-block; padding: 0 10px">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bankaustralia.png" class="icon" height="69px">
+		</div>
+		<div style="display: inline-block; padding: 0 10px">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/thesaturdaypaper.png" class="icon" height="69px">
+		</div>
+		<div style="display: inline-block; padding: 0 10px">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/lush.png" class="icon" height="69px">
+		</div>
+		<div style="display: inline-block; padding: 0 10px">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/theguardian.png" class="icon" height="69px">
+		</div>
+	</div>
+	<div class="footer">
+		&copy; 2019 HRAFF - Human Rights Arts & Film Festival - hraff.org.au. <a href="http://localhost:3157/beta/contact-us">Contact Us</a><a href="http://localhost:3157/beta/privacy-policy">Privacy</a>
+	</div>
   </footer>
   
   <?php wp_footer(); ?>
